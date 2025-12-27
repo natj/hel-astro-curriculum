53768c9e9d9b785b9609c2272738a62942bf465d
natj <nattila.joonas@gmail.com>
2025-12-27 21:06:57 +0200

    teacher database

---
+++ teacherinfo.py
@@ -0,0 +1,292 @@
+
+
+
+teachers = [
+    {
+        "name": "Mika Juvela",
+        "email": "mika.juvela@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Lauri Jetsu",
+        "email": "lauri.jetsu@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Tuomo Salmi",
+        "email": "tuomo.salmi@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Thomas Hackman",
+        "email": "thomas.hackman@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Emilia Kilpua",
+        "email": "emilia.kilpua@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Taina Kurki-Suonio",
+        "email": "taina.kurki-suonio@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Sanni Hoilijoki",
+        "email": "sanni.hoilijoki@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Kai Nordlund",
+        "email": "kai.nordlund@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Mikael Granvik",
+        "email": "mikael.granvik@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Elina Keihänen",
+        "email": "elina.keihanen@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Anne Virkki",
+        "email": "anne.virkki@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Peter Johansson",
+        "email": "peter.johansson@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Jorma Harju",
+        "email": "jorma.harju@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Alexis Finoguenov",
+        "email": "alexis.finoguenov@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Hannu Kurki-Suonio",
+        "email": "hannu.kurki-suonio@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Kenneth Österberg",
+        "email": "kenneth.osterberg@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Till Sawala",
+        "email": "till.sawala@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Antti Penttilä",
+        "email": "antti.penttila@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Minna Palmroth",
+        "email": "minna.palmroth@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Urs Ganse",
+        "email": "urs.ganse@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Jens Pomoell",
+        "email": "jens.pomoell@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Daniel Price",
+        "email": "daniel.price@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Ranadeep Sarkar",
+        "email": "ranadeep.sarkar@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Eleanna Asvestari",
+        "email": "eleanna.asvestari@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Simon Good",
+        "email": "simon.good@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Lucile Turc",
+        "email": "lucile.turc@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Joonas Nättilä",
+        "email": "joonas.nattila@helsinki.fi",
+        "webpage": "https://natj.github.io/",
+        "title": "",
+        "description": "Associate Professor of Plasma Astrophysics"
+    },
+    {
+        "name": "Clif Kirkpatrick",
+        "email": "clif.kirkpatrick@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Francisco Garcia Fuentes",
+        "email": "francisco.garciafuentes@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Jens Brücken",
+        "email": "jens.brucken@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Filip Tuomisto",
+        "email": "filip.tuomisto@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Sami Lehti",
+        "email": "sami.lehti@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Matti Heikinheimo",
+        "email": "matti.heikinheimo@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Mikko Voutilainen",
+        "email": "mikko.voutilainen@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Anca Tureanu",
+        "email": "anca.tureanu@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Oleg Lebedev",
+        "email": "oleg.lebedev@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Syksy Räsänen",
+        "email": "syksy.rasanen@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Eric MacLennan",
+        "email": "eric.maclennan@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Karri Muinonen",
+        "email": "karri.muinonen@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Yehor Surkov",
+        "email": "yehor.surkov@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    },
+    {
+        "name": "Zuri Gray",
+        "email": "zuri.gray@helsinki.fi",
+        "webpage": "",
+        "title": "",
+        "description": ""
+    }
+]
